06155e7cb5f4c3ec75b2917212b68b96d06acfa1
Dominic Holt <domholt@gmail.com>
2014-09-22 12:08:46 -0700

    Initial check-in

---
+++ src/com/lmco/test/SpringBootTestServlet.java
@@ -0,0 +1,13 @@
+package com.lmco.test;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class SpringBootTestServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
